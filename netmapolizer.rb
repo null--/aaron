2727ba31f4e1f995d91d20fb44b14830d4f94c06
null-- <sinahatef.cpp@gmail.com>
2014-03-30 17:43:50 +0430

    ltl chng

--- netmapolizer.rb
+++ netmapolizer.rb
@@ -18,14 +18,14 @@ class Netmapolizer < Thor
     
     puts <<-BANNER
 Examples:
-  1. Create a new diagram from a netstat output file, then generate report in png and pdf formats
-    ./netmap.rb file netstat.out --verbose --png --pdf --output test.nmg
+  1. Print all windows clients connected to 192.168.0.1 on port 22
+    ./netmapolizer.rb search --dst 192.168.0.1 --dst-port 22 --src-os win
     BANNER
   end
   
   class_option :verbose,  :type => :boolean, :default => false, :alias => "-v",
       :desc => "verbose mode"
-  class_option :nsgfile,  :type => :string, :default => false, :alias => "-i", :required => true
+  class_option :nsgfile,  :type => :string, :default => false, :alias => "-i", :required => true,
       :desc => "verbose mode"
       
   desc "search", "Search something! (e.g. all windows clients connected to 192.168.0.1 on port 22)"
