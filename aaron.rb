a8503d9159857b55cd335165960aebd744a57982
null-- <sinahatef.cpp@gmail.com>
2014-05-28 10:08:48 +0430

    sht

--- aaron.rb
+++ aaron.rb
@@ -15,7 +15,7 @@
 # GNU General Public License for more details.
 #  
 # You should have received a copy of the GNU General Public License
-# along with Graviton.  If not, see http://www.gnu.org/licenses/.
+# along with aaron.  If not, see http://www.gnu.org/licenses/.
 
 # * aaron Project
 
